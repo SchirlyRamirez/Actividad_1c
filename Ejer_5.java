58816155e87e5232847d81aaec8b6865c1864779
Shirley <shirly.ramirez29077@ucaldas.edu.co>
2023-09-30 18:10:29 -0500

    actualizacion ejercicio 5

--- Ejer_5.java
+++ Ejer_5.java
@@ -1,18 +1,18 @@
-Ejer_5
 import java.io.PrintStream;
 import java.util.Scanner;
 
-public class Ejer_5 {
+public class ejer_5 {
 
     public static PrintStream Screen = System.out;
     public static Scanner keyboard = new Scanner(System.in);
-
-    public static void main (String[] args){
-        
-        Screen.println("ingresa el valor en pesos");
-        double pesos = keyboard;   
-        Screen.println(pesos);
-
-
-}
-}
+  
+    public static void Ejer} (String[] args){
+        double DOLAR = 4060;
+        double EURO = 4309;
+      
+        Screen.println("Ingresa el valor en pesos");
+        double pesos = keyboard.nextDouble();
+      
+        Screen.println("Pesos -> " + pesos);
+        Screen.println("Dolares -> " + Math.round((pesos/DOLAR) * 100d) /100d);
+        Screen.println("Euros -> " + Math.round((pesos/EURO) *100d) /100d);
